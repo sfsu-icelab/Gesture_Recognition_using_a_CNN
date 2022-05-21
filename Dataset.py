9f4c379e42e8d689a387af68bacb21e41391af30
AmirModan <52586334+AmirModan@users.noreply.github.com>
2022-05-20 17:24:41 -0700

    Removed Shuffling Bugs
    Dataset will not be shuffled during preprocessing to preserve time integrity of sEMG data. Also, fixed bug in which first 5,600 samples of ENTIRE dataset are used in K-Fold validation. This meant only first two gestures being used, resulting in model being reported as more accurate than it is.

--- Dataset.py
+++ Dataset.py
@@ -9,68 +9,76 @@ Main Program for Offline system which reads data, trains CNN model,
 
 """
 
-from DataRetrieval import dataset
+from DataRetrieval import dataset, extractFeatures
 import tensorflow as tf
 import time
-import random
 from CNN import trainData, testData
+
+data = [None] * 8
+label = [None] * 8
 
 if __name__ == "__main__":
     #Fetch EMG Feature data for each gesture
-    data1 = dataset("Dataset/HandGesture01.txt")
-    label1 = [0 for i in range(len(data1))]
-    data2 = dataset("Dataset/HandGesture02.txt")
-    label2 = [1 for i in range(len(data2))]
-    data3 = dataset("Dataset/HandGesture03.txt")
-    label3 = [2 for i in range(len(data3))]
-    data4 = dataset("Dataset/HandGesture04.txt")
-    label4 = [3 for i in range(len(data4))]
-    data5 = dataset("Dataset/HandGesture05.txt")
-    label5 = [4 for i in range(len(data5))]
-    data6 = dataset("Dataset/HandGesture06.txt")
-    label6 = [5 for i in range(len(data6))]
-    data7 = dataset("Dataset/HandGesture07.txt")
-    label7 = [6 for i in range(len(data7))]
-    data8 = dataset("Dataset/HandGesture08.txt")
-    label8 = [7 for i in range(len(data8))]
+    for gesture in range(8):
+        # Extract raw EMG data
+        data[gesture] = dataset("Dataset/HandGesture0" + str(gesture+1) + ".txt")
+        # Extract MAV from raw data
+        #data[gesture] = extractFeatures(data[gesture])
+        label[gesture] = [gesture for i in range(len(data[gesture]))]
     
-    #Combine all data into one set, ignoring 4 gestures for better comparability
-    data = data1 + data2 + data3 + data4
-    label = label1 + label2 + label3 + label4
-    
-    #TODO: REMOVE RANDOMIZATION
-    #Shuffle Dataset for K-Fold Validation
-    temp = list(zip(data, label))
-    random.shuffle(temp)
-    data, label = zip(*temp)
-    
-    #Divide dataset so each fold has 100 samples
-    divisor = 100
-    x = [data[i:i + divisor] for i in range(0, len(data), divisor)]
-    y = [label[i:i + divisor] for i in range(0, len(label), divisor)]
+    # Start computation timing
     init_time = time.time()
+    
+    # Initialize evaluation metrics
     total_acc = 0
     total_loss = 0
+    
+    # Size of each fold for K-Fold cross validation
+    divisor = int(len(data[1]) / 8)
+    
+    # Train and test a model for each of 8 folds
     for i in range(8):
+        
+        # Instatitate empty lists for current fold
         x_train = []
         y_train = []
-        for j in range(i):
-            x_train += x[j]
-            y_train += y[j]
-        for j in range(i+1,8):
-            x_train += x[j]
-            y_train += y[j]
+        x_test = []
+        y_test = []
         
-        x_test = x[i]
-        y_test = y[i]
-        #Creates a new Sequential Neural Net.
+        # Marks the start and end of fold used for testing
+        test_start = i*divisor
+        test_end = (i+1)*divisor
+        
+        for gesture in range(8):
+            # All folds before current fold used for training
+            x_train += data[gesture][0:test_start]
+            y_train += label[gesture][0:test_start]
+            
+            # Current fold used for testing
+            x_test += data[gesture][test_start:test_end]
+            y_test += label[gesture][test_start:test_end]
+            
+            # All folds after current fold used for training
+            x_train += data[gesture][test_end:]
+            y_train += label[gesture][test_end:]
+        
+        #Creates a new Sequential Neural Net for this fold.
         #"Sequential" refers to the fact that neural layers are stacked in sequence
         model = tf.keras.models.Sequential()
-        model = trainData(model, x_train, y_train, 8, 4)
-        loss, acc = testData(model, x_test, y_test, 8)
+        
+        # Train model using raw sEMG image
+        model = trainData(model, x_train, y_train, 8)
+        # Evaluate current model and update overall evaluation with results
+        loss, acc = testData(model, x_test, y_test)
+        
+        # Train model using features
+        #model = trainData(model, x_train, y_train, 8, 8)
+        # Evaluate current model and update overall evaluation with results
+        #loss, acc = testData(model, x_test, y_test, 8)
+        
         total_acc += acc
         total_loss += loss
         
-        
+    # Print overall evaluation results
     print("Final loss: ", total_loss/8,"\nFinal accuracy: ", total_acc/8)
     print("Time taken to train Net: "+str(time.time()-init_time)+" seconds\n")
